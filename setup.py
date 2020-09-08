7190ed59a39c917df03cf0efa4a4b16489228e10
Ben Lindsay <ben.lindsay@chrobinson.com>
2020-09-07 20:57:05 -0500

    add requirements to setup.py

--- setup.py
+++ setup.py
@@ -4,45 +4,49 @@
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
+with open("README.rst") as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-requirements = [ ]
+requirements = ["pandas>=1.0.0", "plotly>=4.0.0"]
 
-setup_requirements = ['pytest-runner', ]
+setup_requirements = [
+    "pytest-runner",
+]
 
-test_requirements = ['pytest>=3', ]
+test_requirements = [
+    "pytest>=3",
+]
 
 setup(
     author="Ben Lindsay",
-    author_email='benjlindsay@gmail.com',
-    python_requires='>=3.5',
+    author_email="benjlindsay@gmail.com",
+    python_requires=">=3.5",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     description="Enables plotting ECDF curves using Plotly with a similar API to Plotly Express",
     install_requires=requirements,
     license="MIT license",
-    long_description=readme + '\n\n' + history,
+    long_description=readme + "\n\n" + history,
     include_package_data=True,
-    keywords='plotly_ecdf',
-    name='plotly_ecdf',
-    packages=find_packages(include=['plotly_ecdf', 'plotly_ecdf.*']),
+    keywords="plotly_ecdf",
+    name="plotly_ecdf",
+    packages=find_packages(include=["plotly_ecdf", "plotly_ecdf.*"]),
     setup_requires=setup_requirements,
-    test_suite='tests',
+    test_suite="tests",
     tests_require=test_requirements,
-    url='https://github.com/benlindsay/plotly_ecdf',
-    version='0.1.0',
+    url="https://github.com/benlindsay/plotly_ecdf",
+    version="0.1.0",
     zip_safe=False,
 )
